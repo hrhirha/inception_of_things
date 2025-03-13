542b93e132249a3c02393014fdc8949799d7a6fe
Your Name <you@example.com>
2025-03-13 14:01:27 +0000

    fix

--- bonus/scripts/setup-k8s-cluster.sh
+++ bonus/scripts/setup-k8s-cluster.sh
@@ -27,7 +27,7 @@ create_resources() {
 
     echo "Starting port forwarding..."
     sudo kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8181:443 &
-    while sudo kubectl get pods -n dev | grep -q 'No resources found in dev namespace'; do
+    while sudo kubectl get pods -n dev 2>&1 | grep -q 'No resources found in dev namespace'; do
 	sleep 5
     done
     sudo kubectl wait --for=condition=ready --timeout=300s pod --all -n dev # Allow time for App to initialize
